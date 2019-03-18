13ae1c24d2766c6d7aac34a15fd0f30d2f19c7cb
donfred <donfredeveloper@gmail.com>
2019-03-18 11:51:02 +0100

    first commit

---
+++ src/main/java/com/technical/testing/raceresult/App.java
@@ -0,0 +1,13 @@
+package com.technical.testing.raceresult;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
